346b74779b1e2c3e5cbc138f48c008e0c44a3879
Nuzul Zaif Mahdiono Ramadhan <40730150+nuzulr24@users.noreply.github.com>
2020-09-30 13:17:53 +0700

    udah update ni syg
    langsung aja dipull syg

--- application/views/templates/theme.php
+++
@@ -1,8 +0,0 @@
-<div id="app-settings">
-    <app-settings theme-active="teal-dark" :theme-location="{
-  'blue-light': 'http://tutorio-bootstrap.frontendmatter.com/login.html',
-  'blue-dark': 'http://tutorio-dark.frontendmatter.com/login.html',
-  'teal-light': 'http://tutorio-teal.frontendmatter.com/login.html',
-  'teal-dark': 'http://tutorio-teal-dark.frontendmatter.com/login.html'
-}" sidebar-type="dark" sidebar-variant="bg-transparent border-0"></app-settings>
-</div>
